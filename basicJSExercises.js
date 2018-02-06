707ce4c564185be67b22b12d1a3d13280c908f74
VladPerish <perish.abs@gmail.com>
2018-02-07 00:47:33 +0600

    complete

--- basicJSExercises.js
+++ basicJSExercises.js
@@ -121,121 +121,171 @@ function remove_first_occurrence(text, searchstring) {
 }
 
 console.log(remove_first_occurrence("the quick brown fox jumps over the lazy dog", 'the'));
-/* Uncomment the following to check */
-  //console.log(remove_first_occurrence("The quick brown fox jumps over the lazy dog", 'the'));
-  //console.log(remove_first_occurrence("Drastic times call for drastic measures", 'drastic'));
 
 
 /* ---------------------------
 *** #6 Alphabetical Order ***
-
 Write a JavaScript function that returns a passed string with letters in alphabetical order.
-
 Test:
 console.log(alphabetic_order('textbook'));
-
 Output:
 bekoottx
-
 HINT: Use join(), split() and sort() functions
  --------------------------- */
 
 function alphabetic_order(word) {
-  return "rearranged word";
+    var arr , splited ,sorted;
+
+      arr = word.split("");
+      arr.sort();
+      sorted=arr.join("");
+
+  return sorted;
 }
 
 console.log("Alphabetic Order:");
 /* Uncomment the following to check */
-  // console.log(alphabetic_order("textbook"));
-  // console.log(alphabetic_order("webmaster"));
-  // console.log(alphabetic_order("supercalifragilisticexpialidocious"));
+   console.log(alphabetic_order("textbook"));
+   console.log(alphabetic_order("webmaster"));
+   console.log(alphabetic_order("supercalifragilisticexpialidocious"));
 
 
 /* ---------------------------
 *** #7 Most Frequent Item ***
-
 Write a JavaScript function to find the most frequent item in a given array.
-
 Test:
 most_frequent([3, 'c', 'c', 'c', 2, 3, 'c', 3, 'c', 2, 4, 9, 3]);
-
 Output:
 c occurs 5 times
  --------------------------- */
 
 function most_frequent(arr) {
+
+var mostFrequent = 1;
+var n= 0;
+var item;
+for (var i=0; i<arr.length; i++)
+{
+        for (var j=i; j<arr.length; j++)
+        {
+                if (arr[i] == arr[j])
+                 n++;
+                if (mostFrequent<n)
+                {
+                  mostFrequent=n;
+                  item = arr[i];
+                }
+         }
+         n=0;
+       }
+
   console.log("Most frequently occuring item in arr");
+  console.log(item+" ( " +mostFrequent +" times ) ") ;
 }
 
 console.log("Most Frequent Item:");
 /* Uncomment the following to check */
-  // most_frequent([3, 'c', 'c', 'c', 2, 3, 'c', 3, 'c', 2, 4, 9, 3]);
-  // most_frequent([7, 2, 'ax', '9', 9, 'ax', 'ax']);
+  most_frequent([3, 'c', 'c', 'c', 2, 3, 'c', 3, 'c', 2, 4, 9, 3]);
+  most_frequent([7, 2, 'ax', '9', 9, 'ax', 'ax']);
 
 
 /* ---------------------------
 *** #8 Remove Duplicate Values ***
-
 Write a JavaScript program to find and remove duplicate values in a JavaScript array.
-
 Test:
 remove_duplicates([3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3]);
-
 Output:
 [3, 'a', 2, 4, 9]
  --------------------------- */
 
 function remove_duplicates(arr) {
   console.log("Duplicates removed from array");
-}
+    var x, len=arr.length, out=[], obj={};
+
+  for (x=0; x<len; x++) {
+    obj[arr[x]]=0;
+  }
+  for (x in obj) {
+    out.push(x);
+  }
+  console.log(out);
+
+   }
 
 console.log("Remove Duplicate Values:");
 /* Uncomment the following to check */
-  // remove_duplicates([3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3]);
-  // remove_duplicates([4, 4, 4, 5, 's', 8, 's']);
+   remove_duplicates([3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3]);
+   remove_duplicates([4, 4, 4, 5, 's', 8, 's']);
 
 
 /* ---------------------------
 *** #9 Dash between Even Numbers ***
-
 Write a JavaScript program which accepts a number as input and inserts dashes (-) between two consecutive even numbers.
-
 Test:
 dash_in_even(012345684);
-
 Output:
 "012-456-8-4"
  --------------------------- */
 
 function dash_in_even(number) {
   console.log("even numbers separated by dashes");
+    var num=number;
+    var str = num.toString();
+    var result = [str[0]];
+
+    for(var x=1; x<str.length; x++)
+      {
+        if((str[x-1]%2 === 0)&&(str[x]%2 === 0))
+         {
+          result.push('-', str[x]);
+         }
+        else
+         {
+          result.push(str[x]);
+         }
+      }
+    console.log(result.join(''));
 }
 
 console.log("Dash between Even Numbers:");
 /* Uncomment the following to check */
-  // dash_in_even(100);
-  // dash_in_even(1356);
-  // dash_in_even(246824);
-  // dash_in_even(1324567824);
+   dash_in_even(100);
+   dash_in_even(1356);
+   dash_in_even(246824);
+   dash_in_even(1324567824);
 
 
 /* ---------------------------
 *** #10 Guessing Game ***
-
 Write a JavaScript program where the program takes a random integer between 1 to 10,
 the user is then prompted to input a guess number. If the user input matches with guess number,
 the program will display a message "Good Work" otherwise display a message "Not matched"
-
 HINT: Use Math.ceil() and Math.random()
  --------------------------- */
 
 function guessing_game(guess) {
   // Get a random integer from 1 to 10 inclusive
   console.log("matched or unmatched?");
+    var num;
+
+      num = Math.random() * 100;
+      num = Math.ceil(num);
+      num = (num % 10) + 1;
+      console.log("When guess = " + guess + ", num = " + num);
+
+    if(guess == num)
+    {
+      console.log("Good Work");
+    }
+
+    else
+    {
+      console.log("Not matched");
+    }
 }
 
 console.log("Guessing Game:");
 /* Uncomment the following to check */
-  // var guess = prompt('Guess the number between 1 and 10 inclusive');
-  // console.log("User guessed: "+ guess);
-  // guessing_game(guess);
+   var guess = prompt('Guess a number between 1 and 10');
+   console.log("User guessed: "+ guess);
+guessing_game(guess);
